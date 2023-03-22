08ec52a895ea6e1fb8e96f90c0c399302b1b1f75
lachkar99zakaria <lachkar99zakaria@gmail.com>
2023-03-22 08:56:02 +0000

    Initial Commit

---
+++ IAmRichSwiftUi/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  IAmRichSwiftUi
+//
+//  Created by zakaria lachkar on 22/03/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ IAmRichSwiftUi/IAmRichSwiftUiApp.swift
@@ -0,0 +1,17 @@
+//
+//  IAmRichSwiftUiApp.swift
+//  IAmRichSwiftUi
+//
+//  Created by zakaria lachkar on 22/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct IAmRichSwiftUiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
